f489d2edb14601c34a6274fc29d7f592fe4f60c1
lobnaHosny <45228631+lobnaHosny@users.noreply.github.com>
2019-12-19 12:18:31 +0800

    Remove volatile

--- src/test/java/ChatServer/src/Server/ServerTest.java
+++ src/test/java/ChatServer/src/Server/ServerTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ServerTest {
 
-    public volatile Server server;
+    public Server server;
     @BeforeEach
     void runServer(){
         server = new Server(9000);
